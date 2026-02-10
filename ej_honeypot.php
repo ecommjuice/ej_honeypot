ef75c7fbfbfdd27a5ae58cd6e4b0cdc3f6727adc
EcommJuice <54352980+ecommjuice@users.noreply.github.com>
2026-02-10 11:57:40 +0100

    Update ej_honeypot.php Backoffice

--- ej_honeypot.php
+++ ej_honeypot.php
@@ -9,7 +9,7 @@ class Ej_Honeypot extends Module
     {
         $this->name = 'ej_honeypot';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.1.0';
         $this->author = 'EJ Agencia';
         $this->need_instance = 0;
         $this->bootstrap = true;
@@ -17,48 +17,143 @@ class Ej_Honeypot extends Module
         parent::__construct();
 
         $this->displayName = $this->l('EJ Honeypot Anti-Spam');
-        $this->description = $this->l('Protege tus formularios de bots usando la técnica Honeypot.');
+        $this->description = $this->l('Configura protección invisible contra bots en tus formularios.');
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
     }
 
     public function install()
     {
+        // Valores por defecto: todos activados al instalar
+        Configuration::updateValue('EJ_HONEYPOT_CONTACT', 1);
+        Configuration::updateValue('EJ_HONEYPOT_REGISTER', 1);
+        Configuration::updateValue('EJ_HONEYPOT_NEWSLETTER', 1);
+
         return parent::install() &&
             $this->registerHook('displayHeader') &&
             $this->registerHook('displayContactForm') &&
             $this->registerHook('displayCustomerAccountForm') &&
+            $this->registerHook('displayNewsletterRegistration') &&
             $this->registerHook('actionContactFormSubmitBefore') &&
-            $this->registerHook('actionCustomerRegisterSubmitBefore');
+            $this->registerHook('actionCustomerRegisterSubmitBefore') &&
+            $this->registerHook('actionNewsletterRegistrationBefore');
     }
 
-    // Insertamos el campo oculto en los formularios
+    public function uninstall()
+    {
+        Configuration::deleteByName('EJ_HONEYPOT_CONTACT');
+        Configuration::deleteByName('EJ_HONEYPOT_REGISTER');
+        Configuration::deleteByName('EJ_HONEYPOT_NEWSLETTER');
+        return parent::uninstall();
+    }
+
+    // --- SECCIÓN DE CONFIGURACIÓN (BACKOFFICE) ---
+
+    public function getContent()
+    {
+        $output = '';
+        if (Tools::isSubmit('submit' . $this->name)) {
+            Configuration::updateValue('EJ_HONEYPOT_CONTACT', (int)Tools::getValue('EJ_HONEYPOT_CONTACT'));
+            Configuration::updateValue('EJ_HONEYPOT_REGISTER', (int)Tools::getValue('EJ_HONEYPOT_REGISTER'));
+            Configuration::updateValue('EJ_HONEYPOT_NEWSLETTER', (int)Tools::getValue('EJ_HONEYPOT_NEWSLETTER'));
+            $output .= $this->displayConfirmation($this->l('Configuración actualizada correctamente.'));
+        }
+
+        return $output . $this->renderForm();
+    }
+
+    public function renderForm()
+    {
+        $fields_form = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->l('Configuración de Formularios'),
+                    'icon' => 'icon-cogs',
+                ],
+                'input' => [
+                    [
+                        'type' => 'switch',
+                        'label' => $this->l('Proteger Formulario de Contacto'),
+                        'name' => 'EJ_HONEYPOT_CONTACT',
+                        'is_bool' => true,
+                        'values' => [['id' => 'active_on', 'value' => 1], ['id' => 'active_off', 'value' => 0]],
+                    ],
+                    [
+                        'type' => 'switch',
+                        'label' => $this->l('Proteger Registro de Clientes'),
+                        'name' => 'EJ_HONEYPOT_REGISTER',
+                        'is_bool' => true,
+                        'values' => [['id' => 'active_on', 'value' => 1], ['id' => 'active_off', 'value' => 0]],
+                    ],
+                    [
+                        'type' => 'switch',
+                        'label' => $this->l('Proteger Suscripción Newsletter'),
+                        'name' => 'EJ_HONEYPOT_NEWSLETTER',
+                        'is_bool' => true,
+                        'values' => [['id' => 'active_on', 'value' => 1], ['id' => 'active_off', 'value' => 0]],
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->l('Guardar'),
+                    'class' => 'btn btn-default pull-right'
+                ],
+            ],
+        ];
+
+        $helper = new HelperForm();
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
+        $helper->fields_value['EJ_HONEYPOT_CONTACT'] = Configuration::get('EJ_HONEYPOT_CONTACT');
+        $helper->fields_value['EJ_HONEYPOT_REGISTER'] = Configuration::get('EJ_HONEYPOT_REGISTER');
+        $helper->fields_value['EJ_HONEYPOT_NEWSLETTER'] = Configuration::get('EJ_HONEYPOT_NEWSLETTER');
+
+        return $helper->generateForm([$fields_form]);
+    }
+
+    // --- LÓGICA DE FILTRADO ---
+
     public function hookDisplayContactForm()
     {
-        return $this->display(__FILE__, 'views/templates/hook/honeypot.tpl');
+        if (Configuration::get('EJ_HONEYPOT_CONTACT')) {
+            return $this->display(__FILE__, 'views/templates/hook/honeypot.tpl');
+        }
     }
 
     public function hookDisplayCustomerAccountForm()
     {
-        return $this->display(__FILE__, 'views/templates/hook/honeypot.tpl');
+        if (Configuration::get('EJ_HONEYPOT_REGISTER')) {
+            return $this->display(__FILE__, 'views/templates/hook/honeypot.tpl');
+        }
     }
 
-    // Validación para el formulario de contacto
+    public function hookDisplayNewsletterRegistration()
+    {
+        if (Configuration::get('EJ_HONEYPOT_NEWSLETTER')) {
+            return $this->display(__FILE__, 'views/templates/hook/honeypot.tpl');
+        }
+    }
+
     public function hookActionContactFormSubmitBefore()
     {
-        $this->validateHoneypot();
+        if (Configuration::get('EJ_HONEYPOT_CONTACT')) { $this->validateHoneypot(); }
     }
 
-    // Validación para el formulario de registro
     public function hookActionCustomerRegisterSubmitBefore()
     {
-        $this->validateHoneypot();
+        if (Configuration::get('EJ_HONEYPOT_REGISTER')) { $this->validateHoneypot(); }
+    }
+
+    public function hookActionNewsletterRegistrationBefore()
+    {
+        if (Configuration::get('EJ_HONEYPOT_NEWSLETTER')) { $this->validateHoneypot(); }
     }
 
     private function validateHoneypot()
     {
-        // Si el campo "ej_honeypot_field" tiene contenido, es un bot
         if (Tools::getValue('ej_honeypot_field') && Tools::getValue('ej_honeypot_field') != "") {
-            die("Spam detected. Request blocked.");
+            // Log opcional: podrías registrar aquí el ataque
+            die("Spam protection triggered.");
         }
     }
 }
